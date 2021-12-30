dbedd628d996e11f75c3a3b1348c71244da8e9e7
Toshiyuki Hara <thara0402@gmail.com>
2021-12-30 21:49:47 +0900

    enable appsetting.json.

--- FunctionApp/Startup.cs
+++ FunctionApp/Startup.cs
@@ -30,8 +30,8 @@ namespace FunctionApp
             {
                 var config = builder.ConfigurationBuilder.Build();
                 builder.ConfigurationBuilder
-//                        .AddAzureKeyVault(new Uri(config["KeyVaultUrl"]), new DefaultAzureCredential());
-                        .AddAzureKeyVault(new Uri("https://gunners-style.vault.azure.net/"), new DefaultAzureCredential());
+                        .AddAzureKeyVault(new Uri(config["KeyVaultUrl"]), new DefaultAzureCredential());
+//                        .AddAzureKeyVault(new Uri("https://gunners-style.vault.azure.net/"), new DefaultAzureCredential());
             }
         }
     }
